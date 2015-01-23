41bfc11945f9b44131c13b9d706b5548ea8798fb
Lucas <lucas.loliveira@hotmail.com>
2015-01-23 20:21:48 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+YepinessApi::Application.config.secret_token = 'c11b88dff163963278bc9eb5674460d9cf214b3940fb653165cd76e87c2ae2e101202b8e77fc7786dbfb9c1b758adcf770f1fda269d8c34a69e9b258cfe86209'
