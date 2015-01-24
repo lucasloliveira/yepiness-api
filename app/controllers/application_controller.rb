07c97da2084f86542fcba8a4277902c1d04d9487
Lucas <lucas.loliveira@hotmail.com>
2015-01-24 16:04:53 -0200

    Got login to work w/ client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+  include ActionController::MimeResponds
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
+
+  private
+  def record_not_unique
+    render :json => {:error => "record_not_unique"}, :status => 400
+  end
 end
