ae2223d85e1876438dc5c6bcd93a3647dc3ba0cd
Lucas <lucas.loliveira@hotmail.com>
2015-04-04 12:40:19 -0300

    Fixing yep creation w/ user friends

--- app/controllers/yep_controller.rb
+++ app/controllers/yep_controller.rb
@@ -5,6 +5,11 @@ class YepController < ApplicationController
   def create
     yep = Yep.new(yep_params)
     yep.user = current_user
+    friends = params[:newYep][:friends]
+
+    friends.each do |friend|
+      yep.users << User.find(friend[:id])
+    end
 
     # TODO: Implement relation between users and yeps
     # yep.users << current_user
@@ -17,16 +22,21 @@ class YepController < ApplicationController
 
   def sent
     yeps = Yep.where(user_id: current_user.id)
+    p 'sent'
+    p yeps
 
     render json: yeps.order(created_at: :desc)
   end
 
   def received
-    render json: current_user.yeps.order(created_at: :desc)
+    receivedYeps = Yep.joins(:users).where(users: {id: current_user.id})
+    p 'received'
+    p receivedYeps
+    render json: receivedYeps.order(created_at: :desc)
   end
 
   private
   def yep_params
-    params.require(:newYep).permit(:title, :content, :description, :url, :image)
+    params.require(:newYep).permit(:title, :content, :description, :url, :image, :friends)
   end
 end
